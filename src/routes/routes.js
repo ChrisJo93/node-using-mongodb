cfd15257a89c6ffd173d37eee3a57f699938c1c0
ChristopherJohnson <Christopher.A.Johnson@target.com>
2022-02-21 17:19:10 -0600

    added get route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 import { addnewProduct } from '../controllers/controllers';
+import { getProducts } from '../controllers/controllers';
 
 const routes = (app) => {
   app.route('/products').post(addnewProduct);
+  app.route('/products').get(getProducts);
 };
 
 export default routes;
